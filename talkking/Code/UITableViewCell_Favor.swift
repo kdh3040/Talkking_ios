64b5f5502ea78a75dd595818bba9a8181c393939
mangomilk114 <mangomilk114@gmail.com>
2018-04-24 10:37:04 +0900

    [배환웅] 기본 UI 틀 잡고 있는중

---
+++ talkking/Code/UITableViewCell_Favor.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell_Favor.swift
+//  talkking
+//
+//  Created by mangomilk114 on 2018. 4. 23..
+//  Copyright © 2018년 도호소프트. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UITableViewCell_Favor : UITableViewCell
+{
+    @IBOutlet var Name: UILabel!
+}
