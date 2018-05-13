c8e03081d25843ec3b78943dce57957528f65541
mangomilk114 <mangomilk114@gmail.com>
2018-05-13 17:50:47 +0900

    [배환웅] UI 추가중

---
+++ talkking/UI/Page/UIViewController_ThumbnailList.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController_ThumbnailList.swift
+//  talkking
+//
+//  Created by mangomilk114 on 2018. 5. 13..
+//  Copyright © 2018년 도호소프트. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ImageSlideshow
+
+class UIViewController_ThumbnailList: UIViewController {
+    
+    open override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    @IBAction func Back(_ sender: Any) {
+    }
+    
+    
+}
