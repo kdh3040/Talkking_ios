f7936184cee06e1f5b9304334af7c13fb804c0e9
mangomilk114 <mangomilk114@gmail.com>
2018-05-30 12:27:50 +0900

    [배환웅] 메인페이지 로딩 에러

--- talkking/AppDelegate.swift
+++ talkking/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         CommonUIFunc.ShowLoading()
         
-        FireBaseFunc.Instance.LoadUserIndex(uuid: "a8970cb9531bdd5c" as! String)
+        FireBaseFunc.Instance.LoadUserIndex(uuid: "7df9bcf972c36f24" as! String)
  
  
         //NSString *UUID = [nameofclass]
